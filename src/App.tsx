7248825f75ba78fa2bf1f756e311657fb760289f
DmitryAstapenko <dmitryastapenko1@gmail.com>
2021-03-01 17:01:58 +0300

    feat: add Game component in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Game from './components/game/Game'
 
 function App() {
   return (
     <div className="App">
-      
+      <Game></Game>
     </div>
   );
 }
