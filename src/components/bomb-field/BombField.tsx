71767985a754530e0254af3d822f21b3206d92b4
DmitryAstapenko <dmitryastapenko1@gmail.com>
2021-03-01 16:57:26 +0300

    feat: add basic lyout for BombField component

---
+++ src/components/bomb-field/BombField.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import { ICell } from '../cell/cell-service';
+import Cell from '../cell/Cell';
+import './BombField.css';
+
+export interface IBombFieldProps {
+  cells: ICell[][];
+  width: number;
+  height: number;
+}
+
+export interface IBombFieldState {
+  cells: ICell[][];
+}
+
+export default class BombField extends React.Component<IBombFieldProps, IBombFieldState> {
+  constructor(props: IBombFieldProps) {
+    super(props);
+
+    this.state = {
+      cells: this.props.cells
+    }
+  }
+
+  public render() {
+    const width = this.props.width;
+    const height = this.props.height;
+    const cells: ICell[][] = this.props.cells;
+
+    return (
+      <div className="bomb-field"
+        style={{
+          gridTemplateColumns: `repeat(${width}, 2rem)`, 
+          gridTemplateRows: `repeat(${height}, 2rem)`
+        }}
+      >
+        {cells.flat().map((item, index) => 
+          <Cell key={index.toString()}
+            mode={item.mode}
+            coordinates={item.coordinates}
+            value={item.value}/>
+        )}
+      </div>
+    );
+  }
+}
