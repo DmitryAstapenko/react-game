fab2ed20260640d68c0bed8baf8e567cd8141100
Dmitry Astapenko <dmitryastapenko1@gmail.com>
2021-03-07 13:18:02 +0300

    feat: add footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 import Game from './components/game/Game'
+import RSSchool from './images/rs_school_js.svg';
 
-function App() {
+function App() {    
   return (
-    <div className="App">
+    <div className="App">      
       <Game></Game>
+      <footer>        
+        <div>
+          <a href="https://github.com/DmitryAstapenko">Dzmitry Astapenka</a>
+        </div>
+        <div>
+          <span>2020</span>
+        </div>
+        <div>
+          <a href="https://rs.school/js/">
+            <img src={RSSchool} alt="rs school js" />
+          </a>
+        </div>
+      </footer>
     </div>
   );
 }
